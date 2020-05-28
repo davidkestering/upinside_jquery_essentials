fc5ba7e4b75c253d4b0649b17d0b97510a746d2c
David Kestering <davidkestering@gmail.com>
2020-05-28 16:59:40 -0300

    Aula de manipuladores de conteudo outsite!

---
+++ 03-manipuladores/outside.js
@@ -0,0 +1,16 @@
+$(function () {
+    var title = $("title").text();
+
+    var old_text = "<p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>";
+
+    //colocar texto antes de elemento
+    $(".j").before("<h1>"+title+"</h1>");
+    $("<p>simply dummy text of the printing and typesetting industry</p>").insertBefore($(".j"));
+
+    //colocar texto depois de elemento
+    $(".j").after(old_text);
+
+    $(".j").after("<a class='a' href='#'>Contine Lendo...</a>");
+
+    $(".a").insertAfter("p:last");
+})

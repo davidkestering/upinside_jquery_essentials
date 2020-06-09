ab51707b4896e207c762a44b02950da31746a8ea
David Kestering <davidkestering@gmail.com>
2020-06-09 15:44:17 -0300

    Aula de atravessadores de filtragem por posicao!

---
+++ 04-atravessadores/posicao.js
@@ -0,0 +1,18 @@
+$(function () {
+    $.each($("p"),function (i, el) {
+        var el = $(el);
+        el.html(el.html() + " - " + i);
+    });
+
+    //filtragem por posicao
+    $("p").eq(1).css("color","red");
+    $("p").eq(2).css("color","orange");
+    $("p").first().css("color","silver");
+    $("p").last().css("color","pink");
+
+    //filtragem geral de todos os paragrafos
+    $("p").slice(0,4).css({
+        background: "#000",
+        padding: "10px"
+    });
+});
